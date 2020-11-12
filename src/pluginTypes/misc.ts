1c579555a4795062985b3cb2c1d45a819cbed8ff
Dave Farinelli <d@dfar.io>
2020-11-12 14:47:07 -0500

    Completed Misc

--- src/pluginTypes/misc.ts
+++ src/pluginTypes/misc.ts
@@ -12,23 +12,6 @@ export class Misc extends PluginType {
     }
 
     methods(): string {
-        return `/// <summary>
-        /// Check discount requirement
-        /// </summary>
-        /// <param name="request">Object that contains all information required to check the requirement (Current customer, discount, etc)</param>
-        /// <returns>Result</returns>
-        public DiscountRequirementValidationResult CheckRequirement(DiscountRequirementValidationRequest request)
-        {
-            if (request == null)
-                throw new ArgumentNullException(nameof(request));
-    
-            //invalid by default
-            var result = new DiscountRequirementValidationResult();
-
-            // custom logic goes here
-    
-            return result;
-        }        
-`
+        return ``
     }
 }
